66afdfcb2f6a6e8e3c2ee68542398c1ab29c0c02
GabrielVerwt <ryang6361@gmail.com>
2024-09-18 18:34:38 -0300

    incompleto ainda

--- registro.c
+++ registro.c
@@ -1,21 +1,43 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define MAXIMO 100
+
+struct aln {
+    char nome[100];
+    int idade;
+    int matricula;
+};
+
+typedef struct aln Aluno;
+
+void incluir (Aluno a[MAXIMO], int *contador) {
+    
+    if (*contador >= MAXIMO) {
+        printf("Quantidade maxima excedida!\n");
+        return;
+    }
+    Aluno novoAluno;
+    printf("");
+
+    (*contador++);
+}
 
 int main ()
 {
     int opcao;
+
     do {
         system("cls");
         printf("\n----------------------------------------------------------\n");
         printf("\tGerenciamento de cadastro de alunos\t");
         printf("\n----------------------------------------------------------\n");
 
-        printf("Digite sua opcao: \n");
-        printf("1: Cadastrar aluno\n");
+        printf("1: Incluir aluno\n");
         printf("2: Consultar aluno\n");
         printf("3: Alterar dados do aluno\n");
         printf("4: Excluir aluno\n");
         printf("5: Sair\n");
+        printf("Digite sua opcao: ");
         scanf("%d", &opcao);
 
         switch (opcao)
